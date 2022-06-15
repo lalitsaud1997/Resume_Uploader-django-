6bf2cf1c61f32655d240d4b9dd4536b02b143539
lalit saud <lalitsaud1997@gmail.com>
2022-06-15 11:02:15 +0545

    form and post create

--- myapp/views.py
+++ myapp/views.py
@@ -9,3 +9,9 @@ class HomeView(View):
     def get(self, request):
         form = ResumeForm()
         return render(request, 'myapp/home.html', {'form':form})
+
+    def post(self, request):
+        form = ResumeForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return render(request, 'myapp/home.html', {'form':form})
